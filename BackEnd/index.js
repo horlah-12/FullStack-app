65b00599d7900be335357083c64bd8a2d82a13f8
horlah12 <jfash21@gmail.com>
2026-02-02 21:49:24 +0100

    update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -36,7 +36,7 @@ app.use('/api', taskRouter);
 
 app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
 });
 
