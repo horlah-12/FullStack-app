eabb538fdfa4f6ac9aa0e458eb3a471dc654f494
horlah12 <jfash21@gmail.com>
2026-02-02 21:09:58 +0100

    see2

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,21 +12,20 @@ import path from 'path';
 dotenv.config();
 
 
-
-const port = process.env.PORT || 3000;
-
 const app = express();
 /*const port = process.env.PORT*/
 
 // Middleware to enable CORS
 const __dirname = path.resolve(); // required for ES modules
+const port = process.env.PORT || 3000;
 
-
-app.use(cors({
-  origin: "*", // for testing, or your frontend URL for production
-}));
+app.use(cors({ origin: "*"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', taskRouter);
+
+app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
 
 app.use((req, res, next) => {
@@ -37,29 +36,15 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));; // Vite build folder
-
-
-app.use('/api', taskRouter);
-
-
-// Catch-all route to serve index.html for any unknown route
-
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
 })
-
-
 
 //Connect to MongoDB
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {console.log('Connected to MongoDB');})
 .catch((error) => {console.error('Error connecting to MongoDB:', error);});
-
-
 
 
 
